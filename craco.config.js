6576acb979999b9a40a14bdbbfe0f49e39031e63
cola639 <337507950@qq.com>
2022-11-14 11:13:46 +0800

    add: babel-plugin-styled-components

--- craco.config.js
+++ craco.config.js
@@ -1,28 +1,40 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const WebpackBar = require('webpackbar');
-const CracoAlias = require('craco-alias');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+const WebpackBar = require('webpackbar')
+const CracoAlias = require('craco-alias')
 
-process.env.BROWSER = 'none';
+process.env.BROWSER = 'none'
 
 module.exports = {
-  webpack: {
-    plugins: [
-      new WebpackBar({ profile: true }),
-      ...(process.env.NODE_ENV === 'development' ? [new BundleAnalyzerPlugin({ openAnalyzer: false })] : []),
-    ],
-  },
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'tsconfig',
-        // baseUrl SHOULD be specified
-        // plugin does not take it from tsconfig
-        baseUrl: './src/',
-        /* tsConfigPath should point to the file where "baseUrl" and "paths"
-         are specified*/
-        tsConfigPath: './tsconfig.paths.json',
-      },
+    webpack: {
+        plugins: [
+            new WebpackBar({profile: true}),
+            ...(process.env.NODE_ENV === 'development' ? [new BundleAnalyzerPlugin({openAnalyzer: false})] : []),
+        ],
     },
-  ],
-};
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'tsconfig',
+                // baseUrl SHOULD be specified
+                // plugin does not take it from tsconfig
+                baseUrl: './src/',
+                /* tsConfigPath should point to the file where "baseUrl" and "paths"
+         are specified*/
+                tsConfigPath: './tsconfig.paths.json',
+            },
+        },
+    ],
+    babel: {
+        plugins: [
+            [
+                'babel-plugin-styled-components',
+                {
+                    ssr: false,
+                    displayName: process.env.REACT_APP_NODE_ENV !== 'production',
+                    pure: true,
+                },
+            ],
+        ],
+    },
+}
